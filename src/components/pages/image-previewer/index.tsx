cef97c0222514806ece90a036f7275ec8767099e
dsm23 <dsmurdoch357@gmail.com>
2019-04-11 13:42:18 +0100

    Example image in Carousel
    Unsure whether a carousel meets acceptance criteria of an image-previewer. Regardless, it was easy to setup and meets certain demo, look and feel purposes. I had been attempting to fetch the example image but ad-block was having some trouble with it so decided to store in assets folder instead

--- src/components/pages/image-previewer/index.tsx
+++ src/components/pages/image-previewer/index.tsx
@@ -1,81 +1,25 @@
 import * as React from 'react';
-import { useState } from 'react';
 import PropTypes from 'prop-types';
-import {
-  Carousel,
-  CarouselItem,
-  CarouselControl,
-  CarouselIndicators,
-  CarouselCaption,
-} from 'reactstrap';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
 
 type Props = {
   items: {
     src: string,
     altText: string,
-    caption: string,
+    caption?: string,
   }[],
 };
 
-const ImagePreviewer = ({ items }: Props) => {
-  const [activeIndex, setActiveIndex] = useState(0);
-  const [animating, setAnimating] = useState(false);
-
-  const onExiting = () => setAnimating(true);
-
-  const onExited = () => setAnimating(false);
-
-  const next = () => {
-    if (!animating) {
-      const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
-      setActiveIndex(nextIndex);
-    }
-  };
-
-  const previous = () => {
-    if (!animating) {
-      const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
-      setActiveIndex(nextIndex);
-    }
-  };
-
-  const goToIndex = (newIndex: number) => {
-    if (!animating) {
-      setActiveIndex(newIndex);
-    }
-  };
-
-  const slides = items.map(item => (
-    <CarouselItem onExiting={onExiting} onExited={onExited} key={item.src}>
-      <img src={item.src} alt={item.altText} />
-      <CarouselCaption
-        captionText={item.caption}
-        captionHeader={item.caption}
-      />
-    </CarouselItem>
-  ));
-
-  return (
-    <Carousel activeIndex={activeIndex} next={next} previous={previous}>
-      <CarouselIndicators
-        items={items}
-        activeIndex={activeIndex}
-        onClickHandler={goToIndex}
-      />
-      {slides}
-      <CarouselControl
-        direction="prev"
-        directionText="Previous"
-        onClickHandler={previous}
-      />
-      <CarouselControl
-        direction="next"
-        directionText="Next"
-        onClickHandler={next}
-      />
-    </Carousel>
-  );
-};
+const ImagePreviewer = ({ items }: Props) => (
+  <Carousel>
+    {items.map(({ src, altText }) => (
+      <div key={src}>
+        <img src={src} alt={altText} />
+      </div>
+    ))}
+  </Carousel>
+);
 
 ImagePreviewer.propTypes = {
   items: PropTypes.arrayOf(
