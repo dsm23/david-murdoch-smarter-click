bff2fe128afd99b7ae46ba806d64fcc835a89e51
David Murdoch <dsmurdoch357@gmail.com>
2019-04-14 23:12:12 +0100

    Added Sankey (#11)
    Added sankey to display a happy path trail to demonstrate Overlays clicked and overlays shown. Cards added around charts

--- src/components/charts/index.ts
+++ src/components/charts/index.ts
@@ -1,2 +1,3 @@
 export { default as Revenue } from './revenue';
+export { default as OverlaysHappyPath } from './overlays-happy-path';
 export { default as UserImpressions } from './user-impressions';
